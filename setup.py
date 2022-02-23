b05c138426b6d15a12cd12ab204c0a716e2838a1
Seyed Samie Mostafavi <ssmos@kth.se>
2022-02-23 14:31:53 +0000

    setup file added

---
+++ setup.py
@@ -0,0 +1,17 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+setup(
+  name="autoran",
+  version="0.0.1",
+  description="Automatic deployment of open RAN softwares using Python",
+  long_description=README,
+  long_description_content_type="text/markdown",
+  author="Seyed Samie Mostafavi",
+  author_email="samiemostafavi@gmail.com",
+  license="MIT",
+  packages=["autoran"],
+  zip_safe=False
+)
